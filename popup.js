deb1f3218615d1fa1de02ea8228baef70b59113d
yoann <sayo97278@hotmail.fr>
2023-09-08 16:52:48 +0530

    modif tout

--- popup.js
+++ popup.js
@@ -1,4 +1,4 @@
-let seconds = 10;
+let seconds = 0;
 const maClasseElement = document.querySelector("body");
 maClasseElement.style.backdropFilter = "blur";
 
@@ -10,8 +10,6 @@ const newParagraph = document.createElement("modal");
 
 // Ajoutez le paragraphe comme enfant de l'élément avec la classe "maClasse"
 maClasseElement.appendChild(newParagraph);
-
-
 
 // service worker
 if ("serviceWorker" in navigator) {
@@ -42,57 +40,87 @@ navigator.serviceWorker.addEventListener("message", (event) => {
   }
 });
 
+// Sélectionnez l'élément par son ID
+const button = document.querySelector("button");
 
+// Ajoutez un écouteur d'événements de clic
+button.addEventListener("click", function () {
+  resumeTimer();
+});
 
 let intervalId;
-let isTimerActive = false;// le timer est nul 
+let isTimerActive = false; // le timer est nul
 
 function startTimer() {
+  seconds = localStorage.getItem("timerNew");
+  console.log("je suis dans startTimer avec : ", seconds);
   isTimerActive = true; // le décompte commence
-  intervalId = setInterval(function () { //  setInterval arrêter le timer
-    seconds--; // décompte 
+  intervalId = setInterval(function () {
+    //  setInterval arrêter le timer
+    seconds--; // décompte
+    localStorage.setItem("timerNew", seconds); //  clé qui garde la valeur en mémoire, et permet d'enregistrer a l'actualisation
+    console.log("je mets à jour dans startTimer avec : ", seconds);
+
     updateTimerDisplay(seconds);
-    if (seconds === 0) {
-      window.onload = resumeTimer; // fenetre se charge et garde en mémoire les secondes
-      showPopUp() // quand le temps est écoulé la poppup s'ouvre
-      stopTimer() // le temps s'arrete
+    if (seconds === 5 || seconds <= 0) {
+      showPopUp(); // quand le temps est écoulé la poppup s'ouvre
+      stopTimer(); // le temps s'arrete
     }
-    localStorage.setItem("timerSeconds", seconds) //  clé qui garde la valeur en mémoire, et permet d'enregistrer a l'actualisation 
-  },
-    1000); // 1000 = 1 seconde
+  }, 1000); // 1000 = 1 seconde
 }
 
 function stopTimer() {
   isTimerActive = false; // on arrete le timer
   clearInterval(intervalId); // remettre le compteur a zéro
-  localStorage.removeItem("timerSeconds") // supprime de la mémoire
-
+  localStorage.removeItem("timerSeconds"); // supprime de la mémoire faut pas le faire pour partager le timer
 }
 
-function updateTimerDisplay() { // récupère le temps dans le html
-  const timerElement = document.getElementById('timer');
+function updateTimerDisplay() {
+  // récupère le temps dans le html
+  const timerElement = document.getElementById("timer");
   if (timerElement) {
     timerElement.textContent = `Temps de travail : ${seconds} secondes`; // création de texte quand le temps est écoulé
   }
-
 }
 
-function resumeTimer() { // reprends le temps là ou tu t'es arrêté
-  if (!isTimerActive) {// si le timer est différent de faux
-    const storedSeconds = localStorage.getItem("timerSeconds")// garde en mémoire le décompte 
-    if (storedSeconds) {
-      seconds = parseInt(storedSeconds) // timerSeconds est récupéré en chiffre
+function resumeTimer() {
+  console.log("je suis dans resumeTimer");
+  // reprends le temps là ou tu t'es arrêté
+  if (!isTimerActive) {
+    // si le timer est différent de faux
+
+    const storedSecondsNew = localStorage.getItem("timerNew");
+
+    console.log(storedSecondsNew);
+    if (storedSecondsNew > 0) {
+      seconds = parseInt(storedSecondsNew); // timerSeconds est récupéré en chiffre
+      hidePopup();
+
       startTimer(); // le timer repart au moment ou on s'est arrêté
+    } else {
+      localStorage.setItem("timerNew", 20);
+      hidePopup();
+      startTimer();
     }
   }
 }
 
+// storage to set and track timer variables on load
+localStorage.getItem(["timerNew"], (res) => {
+  localStorage.setItem({
+    timerNew: "timerNew" in res ? res.timerNew : 100,
+  });
+});
 window.onload = startTimer; // fenêtre ouverte le timer commence
 
 function showPopUp() {
-  document.getElementById("modal").style.display = "block" // récupère l'élement html modal pour l'afficher en poppup
-  document.getElementById("time").style.display = "none"
+  document.getElementById("modal").style.display = "block"; // récupère l'élement html modal pour l'afficher en poppup
+  document.getElementById("time").style.display = "none";
 }
 
-const modal = document.getElementById("modal")
+function hidePopup() {
+  document.getElementById("modal").style.display = "none"; // récupère l'élement html modal pour l'afficher en poppup
+  document.getElementById("time").style.display = "block";
+}
 
+const modal = document.getElementById("modal");
