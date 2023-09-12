d0de8984273b40e87fcb33c578b5777be2e0eb1b
zineb <zineb.laanaya@gmail.com>
2023-09-12 10:17:09 +0200

    ajout exercice respiration

---
+++ respiration/respiration.js
@@ -0,0 +1,25 @@
+const container = document.getElementById('container');
+const text = document.getElementById('text');
+
+
+const totaltime = 7500;
+const breathTime = (totaltime / 5) * 2;
+const holdTime = totaltime / 5;
+
+breathAnimation();
+
+function breathAnimation() {
+    text.innerText = 'Inspirez!';
+    container.className = 'container grow';
+
+    setTimeout(() => {
+        text.innerText = 'retenez votre respiration!';
+
+        setTimeout(() => {
+            text.innerText = 'Expirez!';
+            container.className = 'container shrink'
+        }, holdTime)
+    }, breathTime)
+}
+
+setInterval(breathAnimation, totaltime);
